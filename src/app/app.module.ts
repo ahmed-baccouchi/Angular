40770adf2eacf8f2bc62a4d3ce2cd80aa325bf92
ahmedbaccouchi <baccouchi.ahmed@esprit.tn>
2026-02-07 07:44:58 +0100

    Worskhop3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
-import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { SuggestionsModule } from './features/suggestions/suggestions.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionComponent
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    CommonModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SuggestionsModule
   ],
   providers: [
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/features/suggestions/suggestions.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuggestionsRoutingModule } from './suggestions-routing.module';
+import { SuggestionsComponent } from './suggestions.component';
+import { SuggestionDetailsComponent } from './suggestion-details/suggestion-details.component';
+import { ListSuggestionComponent } from './list-suggestion/list-suggestion.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    SuggestionsComponent,
+    ListSuggestionComponent,
+    SuggestionDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SuggestionsRoutingModule
+  ]
+})
+export class SuggestionsModule {}
+
